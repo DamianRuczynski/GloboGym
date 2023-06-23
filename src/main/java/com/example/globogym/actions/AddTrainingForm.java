f274bbec3ad3e675f57eac1b8e191774b8321f0a
damian.ruczynski <damian.ruczynski@britenet.com.pl>
2023-06-23 12:30:19 +0200

    add validation on add training

--- src/main/java/com/example/globogym/actions/AddTrainingForm.java
+++ src/main/java/com/example/globogym/actions/AddTrainingForm.java
@@ -3,9 +3,12 @@ package com.example.globogym.actions;
 import com.example.globogym.MainApplication;
 import com.example.globogym.core.TrainingFormController;
 import com.example.globogym.staff.Staff;
+import com.example.globogym.training.Training;
+import core.ActionLogger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 
@@ -13,12 +16,16 @@ import java.net.URL;
 import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
+import static com.example.globogym.actions.editCoachForm.listOfTrainings;
+
 public class AddTrainingForm implements Initializable {
 
     @FXML
     ListView nameListField, roomListField,staffListField;
     @FXML
     DatePicker trainingDateField;
+    @FXML
+    Label message;
     public static String[] trainingNames = new String[]{"abs", "push-ups","running","weightlifting"};
     private void populateFields() {
         for (String name : trainingNames) {
@@ -47,11 +54,32 @@ public class AddTrainingForm implements Initializable {
     }
 
     public void saveForm(){
-        String name = (String) nameListField.getSelectionModel().getSelectedItem();
-        int roomNumber = (int) roomListField.getSelectionModel().getSelectedItem();
-        Staff coach = (Staff) staffListField.getSelectionModel().getSelectedItem();
-        String trainingDate = trainingDateField.getValue().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-        TrainingFormController.saveTraining(name, roomNumber, coach, trainingDate);
+
+        if(nameListField.getSelectionModel().getSelectedItem() != null && roomListField.getSelectionModel().getSelectedItem() != null && staffListField.getSelectionModel().getSelectedItem() != null && trainingDateField.getValue() != null){
+            String name = (String) nameListField.getSelectionModel().getSelectedItem();
+            int roomNumber = (int) roomListField.getSelectionModel().getSelectedItem();
+            Staff coach = (Staff) staffListField.getSelectionModel().getSelectedItem();
+            String trainingDate = trainingDateField.getValue().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            if(checkRoomFree(trainingDate, roomNumber)){
+                message.setText("Training added");
+                TrainingFormController.saveTraining(name, roomNumber, coach, trainingDate);
+            }else{
+                message.setText("Room is reservated for this date frame");
+                ActionLogger.setLog("Cannot assign trainigng to reservated room");
+            }
+        }else{
+            message.setText("Form invalid");
+        }
+
+    }
+
+    private boolean checkRoomFree(String trainingDate, int roomNumber) {
+        for(Training t : listOfTrainings){
+            if(t.getTrainingDateString().equals(trainingDate)) {
+                if(t.getRoomNumber() == roomNumber)return false;
+            }
+        }
+        return true;
     }
 
     @Override
